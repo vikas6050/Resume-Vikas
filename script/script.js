a19205dc33b30f9d7a9172bef468bea213ca78e1
vikas6050 <vikas6050@gmail.com>
2023-09-19 11:49:36 +0530

    added slide show

--- script/script.js
+++ script/script.js
@@ -4,23 +4,19 @@ showSlides();
 function showSlides() {
     let i;
     const slides = document.getElementsByClassName("mySlides");
-
     // Hide all slides
     for (i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";
     }
-
     // Increment the slide index
     slideIndex++;
-
     // Loop back to the first slide if necessary
     if (slideIndex > slides.length) {
         slideIndex = 1;
     }
-
     // Display the current slide
     slides[slideIndex - 1].style.display = "block";
-
     // Change slides every 4 seconds (adjust as needed)
     setTimeout(showSlides, 4000);
 }
+
